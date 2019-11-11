56be50029678b16cb310da5bafca864c66fcaf90
chrzyki <christoph@functor-argument.net>
2019-11-11 12:28:38 +0100

    Refactorings, added setup.cfg, release prep

--- lexibank_wold.py
+++ lexibank_wold.py
@@ -2,7 +2,6 @@ import attr
 from pathlib import Path
 import re
 
-from pylexibank import FormSpec
 from pylexibank import Lexeme, Language
 from pylexibank.providers.clld import CLLD
 from pylexibank.util import progressbar
@@ -11,7 +10,7 @@ from clldutils.misc import slug
 
 
 @attr.s
-class WOLDLexeme(Lexeme):
+class CustomLexeme(Lexeme):
     Word_ID = attr.ib(default=None)
     word_source = attr.ib(default=None)
     Borrowed = attr.ib(default=None)
@@ -35,7 +34,7 @@ class WOLDLexeme(Lexeme):
 
 
 @attr.s
-class WOLDLanguage(Language):
+class CustomLanguage(Language):
     Longitude = attr.ib(default=None)
     Latitude = attr.ib(default=None)
     ISO639P3code = attr.ib(default=None)
@@ -44,17 +43,19 @@ class WOLDLanguage(Language):
 
 def normalize_text(text):
     text = text.replace("\n", " // ")
-    text = re.sub("\s+", " ", text).strip()
+    text = re.sub(r"\s+", " ", text).strip()
 
     return text
 
 
 class Dataset(CLLD):
-    __cldf_url__ = "http://cdstar.shh.mpg.de/bitstreams/EAEA0-92F4-126F-089F-0/wold_dataset.cldf.zip"
+    __cldf_url__ = (
+        "http://cdstar.shh.mpg.de/bitstreams/EAEA0-92F4-126F-089F-0/wold_dataset.cldf.zip"
+    )
     dir = Path(__file__).parent
     id = "wold"
-    lexeme_class = WOLDLexeme
-    language_class = WOLDLanguage
+    lexeme_class = CustomLexeme
+    language_class = CustomLanguage
 
     def cmd_makecldf(self, args):
         # add the bibliographic sources
@@ -82,10 +83,7 @@ class Dataset(CLLD):
         # TODO: Integrate to Concepticon
         for parameter in self.raw_dir.read_csv("parameters.csv", dicts=True):
             if parameter["ID"] not in concept_lookup:
-                concept_id = "%s_%s" % (
-                    parameter["ID"].replace("-", ""),
-                    slug(parameter["Name"]),
-                )
+                concept_id = "%s_%s" % (parameter["ID"].replace("-", ""), slug(parameter["Name"]))
                 args.writer.add_concept(ID=concept_id, Name=parameter["Name"])
                 concept_lookup[parameter["ID"]] = concept_id
 
@@ -100,15 +98,9 @@ class Dataset(CLLD):
             row["Value"] = row["Form"]
             row["Loan"] = float(row["BorrowedScore"]) > 0.6
             row["original_script"] = normalize_text(row["original_script"])
-            row["comment_on_borrowed"] = normalize_text(
-                row["comment_on_borrowed"]
-            )
+            row["comment_on_borrowed"] = normalize_text(row["comment_on_borrowed"])
             row.pop("Segments")
 
             args.writer.add_form(
-                **{
-                    k: v
-                    for k, v in row.items()
-                    if k in self.lexeme_class.fieldnames()
-                }
+                **{k: v for k, v in row.items() if k in self.lexeme_class.fieldnames()}
             )
--- setup.py
+++ setup.py
@@ -15,6 +15,6 @@ setup(
     include_package_data=True,
     zip_safe=False,
     entry_points={"lexibank.dataset": ["wold=lexibank_wold:Dataset"]},
-    install_requires=["pylexibank>=2.0"],
+    install_requires=["pylexibank>=2.1"],
     extras_require={"test": ["pytest-cldf"]},
 )
